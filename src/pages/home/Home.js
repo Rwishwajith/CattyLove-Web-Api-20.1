eb9c1f73c10e092f0e2ef21c37ce2f01eef0c54f
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2022-04-22 13:14:53 +0530

    Create Home.js

---
+++ src/pages/home/Home.js
@@ -0,0 +1,5 @@
+import React from "react";
+import Feed from "../../components/Feed";
+import Create from "../../components/Create";
+import Button from "@mui/material/Button";
+import { Snackbar } from "@mui/material";
