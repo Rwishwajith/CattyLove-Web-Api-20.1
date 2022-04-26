84c749eb73c0b4ba51b1b929adca2905cd720bcf
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2022-04-26 14:12:32 +0530

    Successful implementation of View All Cats Functionality (Client-side)

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -61,6 +61,18 @@ function Home() {
     return data;
   }
 
+    // Get cat details
+    async function getData() {
+      const response = await fetch("http://localhost:4000/api/cats", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setCats([...data]);
+    }
+  
   return (
     <div>
       {/* Create a new cat button */}
