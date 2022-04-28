474567dad03d19fd8b3e2d757656d6a48574dbc1
Deshiwathsala <deshaniwathsala97@gmail.com>
2022-04-29 04:20:02 +0530

    bug fixes

--- server/server.js
+++ server/server.js
@@ -35,14 +35,4 @@ let logger = (req, res, next) => {
   next();
 };
 
-app.use(logger);
-
-app.use(cors());
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(require("./routes/user-routes"));
-app.use(require("./routes/cat-routes"));
-app.use(require("./routes/auth-routes"));
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-swagger(app);
 
