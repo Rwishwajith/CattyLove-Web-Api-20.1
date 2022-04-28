203408786470a5a64c4f981504cae72bf1ac13ab
Deshiwathsala <deshaniwathsala97@gmail.com>
2022-04-29 04:29:20 +0530

    Bug Fixes

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const swagger = require("./swagger");
+const fs = require('fs');
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 4000;
 
 const getActualRequestDurationInMilliseconds = start => {
   const NS_PER_SEC = 1e9; // convert to nanoseconds
@@ -34,5 +42,34 @@ let logger = (req, res, next) => {
   });
   next();
 };
+app.use(logger);
+
+app.use(cors());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(require("./routes/user-routes"));
+app.use(require("./routes/cat-routes"));
+app.use(require("./routes/auth-routes"));
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+swagger(app);
+
+mongoose
+  .connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      // perform a database connection when server starts
+      // dbo.connectToServer(function (err) {
+      //     if (err) console.error(err);
+
+      // });
+      console.log(`Server is running on port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  });
 
 
